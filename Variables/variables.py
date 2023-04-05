79d41447240779385af7d159095a5668fd9a8dfa
LilAguh <aguezeochoa@gmail.com>
2023-04-05 02:30:18 -0300

    variables

---
+++ Variables/variables.py
@@ -0,0 +1,14 @@
+a = 400
+b = 20
+c = a + b
+
+print(c)
+
+nombre = "Aguh"
+print(nombre)
+
+bienvenida = "Hola " + nombre + ", como estas?"
+print(bienvenida)
+
+concatenacionDeDatos = f"{c} es un dato numerico y {nombre} es mi nombre"
+print(concatenacionDeDatos)
