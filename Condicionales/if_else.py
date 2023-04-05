34c63c48eb20dc17e7b454aa320b0cb26e0b50df
LilAguh <aguezeochoa@gmail.com>
2023-04-05 08:22:06 -0300

    condicionales if else

---
+++ Condicionales/if_else.py
@@ -0,0 +1,17 @@
+
+
+edad = 16
+
+if edad >= 18:
+    print("Mayor de edad")
+else:
+    print("Menor de edad")
+    
+    
+contraseña_almacenada = "hola123"
+contraseña_ingresada = "ola123"
+
+if contraseña_almacenada == contraseña_ingresada:
+    print("Iniciando sesion")
+else:
+    print("contraseña erronea")
