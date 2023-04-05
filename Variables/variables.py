643b15351437fea28849c017d0d30a6e27ff1f99
LilAguh <aguezeochoa@gmail.com>
2023-04-05 03:48:38 -0300

    actualizacion variables

--- Variables/variables.py
+++ Variables/variables.py
@@ -1,3 +1,15 @@
+
+#Definiendo variables
+""" 
+    Mucho muy inportante
+    En Python las variables se declaran en snake_case
+    ej: nombre_de_una_variable
+    Caso JavaScript es mas recomendable declarar en camelCase
+    ej: nombreDeUnaVariable
+    
+    acordate genio
+"""
+
 a = 400
 b = 20
 c = a + b
@@ -7,8 +19,17 @@ print(c)
 nombre = "Aguh"
 print(nombre)
 
+#Concatenacion con operadores
+
 bienvenida = "Hola " + nombre + ", como estas?"
 print(bienvenida)
 
-concatenacionDeDatos = f"{c} es un dato numerico y {nombre} es mi nombre"
-print(concatenacionDeDatos)
+#Concatenacion con f-strings
+
+concatenacion_de_datos = f"{c} es un dato numerico y {nombre} es mi nombre"
+print(concatenacion_de_datos)
+
+# Operadores de pertenencia (in, Not in, is, Not is)
+
+print("ola" in bienvenida)  #True, porque ola esta dentro del string bienvenida
+print("ola" is bienvenida)  #False, porque ola no es el string bienvenida
