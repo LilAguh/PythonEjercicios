2b5ec679de6236ee74b9aa88dfc5e6478a0340f6
LilAguh <aguezeochoa@gmail.com>
2023-04-05 05:52:42 -0300

    operadores aritmeticos

---
+++ Operadores/operadores_aritmeticos.py
@@ -0,0 +1,33 @@
+
+#suma y resta (+ y -)
+suma = 12 + 5
+resta = 12 - 5
+
+print(suma)
+print(resta)
+
+#division y multiplicacion
+multi = 12 * 5
+division = 12 / 5 #la division siempre nos devuelve un float
+
+print(multi)
+print(division)
+
+#potenciacion (exponente)
+exponente = 12 ** 5
+
+print(exponente)
+
+#division baja
+division_baja = 12 // 5
+
+print(division_baja)
+
+#resto o modulo
+resto = 12 % 5
+
+print(resto)
+
+
+tipo_de_dato = type(division)
+print(tipo_de_dato)
