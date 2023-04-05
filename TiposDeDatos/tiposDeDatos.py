60ac4a05a3ec36aa5ac56e04d111327bd5dec586
LilAguh <aguezeochoa@gmail.com>
2023-04-05 02:29:51 -0300

    tipos de datos

---
+++ TiposDeDatos/tiposDeDatos.py
@@ -0,0 +1,13 @@
+"string"
+'string'
+
+"""
+Este tipo de string solo funciona
+dentro de un print o algo parecido
+porque fuera solo es un parrafo comentado"""
+
+400  #numero entero, int
+4.23 #numero con coma, float (siempre con punto)
+
+True  #Verdadero, tipo booleano
+False #Falso, tipo booleano
