c8a3721ab69ff467727700c61e2ec8628495f41a
LilAguh <aguezeochoa@gmail.com>
2023-04-05 08:48:15 -0300

    operadores logicos

---
+++ Operadores/operadores_logicos.py
@@ -0,0 +1,25 @@
+
+#AND
+
+resultado1 = True & True #devuelve True
+resultado2 = True & False #devuelve False
+resultado3 = False & True #devuelve False
+resultado4 = False & False #devuelve False
+
+print(resultado3)
+
+#OR
+
+resultado5 = True | True #devuelve True
+resultado6 = False | True #devuelve True
+resultado7 = True | False #devuelve True
+resultado8 = False | False #devuelve False
+
+print(resultado6)
+
+#NOT
+
+resultado9 = not True #devuelve False
+resultado10 = not False #devuelve True
+
+print(resultado10)
