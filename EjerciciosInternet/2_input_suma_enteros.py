f79401b5bb3047779633d8fa95a6aa4b10089dce
LilAguh <aguezeochoa@gmail.com>
2023-04-06 20:53:15 -0300

    Suma entre 2 numeros enteros

---
+++ EjerciciosInternet/2_input_suma_enteros.py
@@ -0,0 +1,14 @@
+
+# 2_ Escribir un programa que lea dos números enteros desde el teclado y muestre su suma.
+
+print('-------------------------------------------------')
+
+print('Ingresa 2 numeros enteros en consola para sumarlos')
+primer_numero = int(input('Primer numero: '))
+segundo_numero = int(input('Segundo numero: '))
+
+print('-------------------------------------------------')
+
+print(f'El resultado de la suma es {primer_numero + segundo_numero}')
+
+print('-------------------------------------------------')
