5366282f03ca19f3d370294f69109ee98cc64e70
LilAguh <aguezeochoa@gmail.com>
2023-04-06 20:54:35 -0300

    Calculo del area de un circulo

---
+++ EjerciciosInternet/4_input_area_circulo.py
@@ -0,0 +1,16 @@
+
+# 4_ Escribir un programa que calcule el área de un círculo dado su radio.
+
+print('-------------------------------------------------')
+
+print('Para calcular el area de un circulo nececitamos saber su radio en centimetros:')
+numero = int(input(''))
+
+area = 3.14 * (numero * numero)
+    
+print('-------------------------------------------------')
+
+print(f'Si el radio del circulo es de {numero}cm,')
+print(f'Su area es de {area}cm en total.')
+
+print('-------------------------------------------------')
