9beb0a8372bfc7a7114f08c97552d46c63ae6a28
LilAguh <aguezeochoa@gmail.com>
2023-04-06 20:54:57 -0300

    Calculo del area de un triangulo

---
+++ EjerciciosInternet/5_area_triangulo.py
@@ -0,0 +1,18 @@
+
+# 5_ Escribir un programa que calcule el área de un triángulo dado su base y altura.
+
+print('-------------------------------------------------')
+
+print('Para calcular el area de un triangu lo necesitamos saber su base y su altura')
+base = int(input('Medida de la base en centimetros: '))
+altura = int(input('Medida de la altura en centimetros: '))
+
+
+area = (base * altura) / 2
+    
+print('-------------------------------------------------')
+
+print(f'La base del triangulo es de {base}cm y la altura de {altura}cm')
+print(f'Su area es de {area}cm en total.')
+
+print('-------------------------------------------------')
