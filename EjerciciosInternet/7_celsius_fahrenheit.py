d3f59c1a0fa38b79228cde35be39cfaf8deb1a9a
LilAguh <aguezeochoa@gmail.com>
2023-04-06 20:56:06 -0300

    Conversion de grados Celsius a Fahrenheit

---
+++ EjerciciosInternet/7_celsius_fahrenheit.py
@@ -0,0 +1,15 @@
+
+# 6_ Escribir un programa que convierta grados Celsius a grados Fahrenheit.
+
+print('-------------------------------------------------')
+
+print('Ingrese los grados en Celsius para convertirlos a Fahrenheit:')
+celsius = int(input(' '))
+
+fahrenheit = (celsius * 1.8) + 32
+    
+print('-------------------------------------------------')
+
+print(f'{celsius}°Celsius equivalen a {fahrenheit}°Fahrenheit')
+
+print('-------------------------------------------------')
