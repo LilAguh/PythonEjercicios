965071602be91d2d99a936fe27ebccc528111b80
LilAguh <aguezeochoa@gmail.com>
2023-04-06 06:24:26 -0300

    inputs con datos numericos

---
+++ Inputs/inputs_numeros.py
@@ -0,0 +1,10 @@
+
+#ingresar un numero
+numero = input('Ingresa un numero para multiplicarlo por 2:  ')
+
+#multiplicar el numero por 2
+#primero hay que pasarlo a int porque el dato original es un string
+resultado = int(numero) * 2
+
+#mostramos el resultado 
+print(resultado)
