cdc2ee0f36638ed410865aea8a8b4761c039e02c
LilAguh <aguezeochoa@gmail.com>
2023-04-06 20:52:49 -0300

    ejercicio Hola mundo

---
+++ EjerciciosInternet/1_hola_mundo.py
@@ -0,0 +1,8 @@
+
+# 1_ Escribir un programa que imprima "¡Hola, mundo!" en la pantalla.
+
+print("-----------------")
+print("|               |")
+print("| ¡Hola, mundo! |")
+print("|               |")
+print("-----------------")
