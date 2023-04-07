b1c062324a006e8eddf4e262cb3ca6a542e8ddb5
LilAguh <aguezeochoa@gmail.com>
2023-04-07 00:04:32 -0300

    contar cuantas veces se repite una letra en la cadena de texto

---
+++ EjerciciosInternet/9_letras_repetidas_cadena.py
@@ -0,0 +1,13 @@
+
+# 9_ Escribir un programa que lea una cadena de texto desde el teclado y muestre la cantidad de veces que aparece una letra dada en la cadena.
+
+print('-------------------------------------------------')
+
+print('Ingrese un texto:')
+caracteres = len(input(''))
+    
+print('-------------------------------------------------')
+
+print(f'El texto ingresado tiene una longitud de {caracteres} caracteres.')
+
+print('-------------------------------------------------')
