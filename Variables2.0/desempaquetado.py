ad238ab141a6e6df8182d7ee58d14c4c0970f157
LilAguh <aguezeochoa@gmail.com>
2023-04-07 09:55:45 -0300

    desempaquetado de datos

---
+++ Variables2.0/desempaquetado.py
@@ -0,0 +1,12 @@
+
+#Creamos una Tupla
+datos = ('Aguh', 'Violeta')
+
+#Desempaquetamos los datos de esa tupla
+nombre, color = datos
+
+#mostramos el resultado
+print(color)
+
+#Esto funciona con listas, tuplas y conjuntos (no podemos desempaquetar numeros)
+
