a70e63b2e5fba4674d3090fa2df6e6af7f7db9bb
LilAguh <aguezeochoa@gmail.com>
2023-04-07 00:03:49 -0300

    longitud de una cadena de texto

---
+++ EjerciciosInternet/8_longitud_cadena_texto.py
@@ -0,0 +1,13 @@
+
+# 8_ Escribir un programa que lea una cadena de texto desde el teclado y muestre su longitud.
+
+print('-------------------------------------------------')
+
+print('Ingrese un texto:')
+caracteres = len(input(''))
+    
+print('-------------------------------------------------')
+
+print(f'El texto ingresado tiene una longitud de {caracteres} caracteres.')
+
+print('-------------------------------------------------')
